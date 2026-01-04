197230dbf443f206788836e552b83d0c223b88ac
Boris Barbulovski <bbarbulovski@gmail.com>
2026-01-04 18:54:04 +0100

    Cleanup .class files in src dir only.

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-find . -name "*.class" -type f -delete
+find src -name "*.class" -type f -delete
 
 javac -cp "./acf-ls-jar-with-dependencies.jar:./json-20250517.jar" src/com/adobe/editor/cfml/parser/generated/CFParse.java src/Main.java
 
 jar cfe cfml_parser.jar Main -C src Main.class -C src com/adobe/editor/cfml/parser/generated/CFParse.class
 
-find . -name "*.class" -type f -delete
+find src -name "*.class" -type f -delete
